2f312b7431d3e6fb789376c7ea4a81734f8fc98c
strdr4605 <str.dr4605@gmail.com>
2017-06-17 14:13:32 +0300

    add room for socket on connection

--- server.js
+++ server.js
@@ -13,13 +13,14 @@ app.get('/', (req, res) => {
 
 
 io.on('connection', (socket) => {
+  socket.join('faf')
   connections.push(socket)
   console.log('Connected: %s sockets connected', connections.length)
-  io.emit('status', { status: 'Connected', connections: connections.length })
+  io.to('faf').emit('status', { status: 'Connected', connections: connections.length })
 
   socket.on('disconnect', (data) => {
     connections.splice(connections.indexOf(socket), 1)
-    io.emit('status', { status: 'Disconnected', connections: connections.length })
+    io.to('faf').emit('status', { status: 'Disconnected', connections: connections.length })
     console.log('Disconnected: %s sockets connected', connections.length)
   })
 })
